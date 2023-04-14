eea2576e5784089e6ad3d40868fad85768856217
Jaimin Suthar <85049765+Jaimin78@users.noreply.github.com>
2023-04-14 13:57:13 +0000

    Created Notes Home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,38 @@
-import React from "react";
+import React, { useContext } from "react";
+import noteContext from '../context/notes/noteContext';
 
 const Home = () => {
-  return(
+  const context = useContext(noteContext)
+  const { notes, setNotes } = context;
+  return (
     <>
-      <h1>Home</h1>
+      <div className="container my-3">
+        <h1>Add Notes</h1>
+        <form>
+          <div className="mb-3">
+            <label for="exampleInputEmail1" className="form-label">Email address</label>
+            <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+              <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
+          </div>
+          <div className="mb-3">
+            <label for="exampleInputPassword1" className="form-label">Password</label>
+            <input type="password" className="form-control" id="exampleInputPassword1" />
+          </div>
+          <div class="mb-3 form-check">
+            <input type="checkbox" className="form-check-input" id="exampleCheck1" />
+              <label className="form-check-label" for="exampleCheck1">Check me out</label>
+          </div>
+          <button type="submit" className="btn btn-primary">Submit</button>
+        </form>
+      </div>
+      <div className="container my-3">
+        <h1>Notes</h1>
+        {
+          notes.map((note)=>{            
+             return <li>{note.title}</li>
+          })
+        }
+      </div>
     </>
   )
 }
