40ed147eedeb27b6aacafbb1256623ea8622cfc4
Jaimin78 <85049765+Jaimin78@users.noreply.github.com>
2023-04-21 10:03:03 +0000

    Created a separate component for a Note items

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,7 @@
-import React, { useContext } from "react";
-import noteContext from '../context/notes/noteContext';
+import React from 'react';
+import Notes from './Notes';
 
 const Home = () => {
-  const context = useContext(noteContext)
-  const { notes, setNotes } = context;
   return (
     <>
       <div className="container my-3">
@@ -25,14 +23,8 @@ const Home = () => {
           <button type="submit" className="btn btn-primary">Submit</button>
         </form>
       </div>
-      <div className="container my-3">
-        <h1>Notes</h1>
-        {
-          notes.map((note)=>{            
-             return <li>{note.title}</li>
-          })
-        }
-      </div>
+      
+      <Notes />
     </>
   )
 }
